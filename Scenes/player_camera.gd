c1819044ce8fb10cf165c5b4f37bc5f55fca3a7f
Fuabba <leeparham44@gmail.com>
2026-02-16 14:50:55 -0500

    TileMaps added
    added base tile map layers and adjusted camera

--- Scenes/player_camera.gd
+++ Scenes/player_camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-# func _process(_delta):
-#     var target := global_position
-#     var smoothed: Vector2 = global_position.lerp(target, 0.2)
-#     global_position = smoothed.round()
+ #func _process(_delta):
+	#var target := global_position
+	#var smoothed: Vector2 = global_position.lerp(target, 0.2)
+#global_position = smoothed.round()
